a6b048253d9fdcafe6c9a28af6dde8674bddd9e8
Matt Richardson <matt.richardson@octopus.com>
2020-08-19 18:31:22 +1000

    Use older pester syntax

--- windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
+++ windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
@@ -15,18 +15,18 @@ Describe  'installed dependencies' {
 
     It 'has dotnet is installed' {
         dotnet --version | Should Match '3.1.401'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has java is installed' {
       java -version 2>&1 | Select-String -Pattern '14\.0\.2' | Should BeLike "*14.0.2*"
-      $LASTEXITCODE | Should -be 0
+      $LASTEXITCODE | Should be 0
     }
 
     It 'has az is installed' {
       $output = (& az version) | convertfrom-json
       $output.'azure-cli' | Should Be '2.10.1'
-      $LASTEXITCODE | Should -be 0
+      $LASTEXITCODE | Should be 0
     }
 
     It 'has aws is installed' {
@@ -35,53 +35,53 @@ Describe  'installed dependencies' {
 
     It 'has node is installed' {
         node --version | Should Match '12.18.3'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has kubectl is installed' {
         kubectl version --client | Should Match '1.18.8'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has helm is installed' {
         helm version | Should Match '3.3.0'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has terraform is installed' {
         terraform version | Should Match '0.13.0'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has python is installed' {
         python --version | Should Match '3.8.5'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has gcloud is installed' {
         gcloud --version | Select-String -Pattern "305.0.0" | Should BeLike "*305.0.0*"
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has octo is installed' {
         octo --version | Should Match '7.4.1'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has eksctl is installed' {
         eksctl version | Should Match '0.25.0'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'has 7zip installed' {
         $output = (& "C:\Program Files\7-Zip\7z.exe" --help) -join "`n"
         $output | Should Match '7-Zip 19.00'
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
     }
 
     It 'should have installed powershell core' {
         $output = & pwsh --version
-        $LASTEXITCODE | Should -be 0
+        $LASTEXITCODE | Should be 0
         $output | Should -match '^PowerShell 7\.0\.3*'
     }
 }
