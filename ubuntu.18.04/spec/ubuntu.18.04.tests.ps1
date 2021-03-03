6bb7caacf2de7cffcf5fa2ae7b6ab4a78dbbb5f6
Anders Båtstrand <anders.batstrand@dsb.no>
2021-03-03 22:21:56 +0100

    Terraform version 0.14.7

--- ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
+++ ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
@@ -53,7 +53,7 @@ Describe  'installed dependencies' {
 
     # If the terraform version is not the latest, then `terraform version` returns multiple lines and a non-zero return code
     It 'has terraform installed' {
-        terraform version | Select-Object -First 1 | Should -match '0.13.5'
+        terraform version | Select-Object -First 1 | Should -match '0.14.7'
     }
 
     It 'has python3 installed' {
