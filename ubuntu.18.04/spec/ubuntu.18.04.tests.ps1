ef0fa6f9f672ce16891625f38a0d821f9b93b051
Justin Walsh <49404281+Justin-Walsh@users.noreply.github.com>
2021-09-15 15:26:11 -0400

    Tweak test to add 4th field

--- ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
+++ ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
@@ -23,7 +23,7 @@ Describe  'installed dependencies' {
     }
 
     It 'has aws powershell module installed' {
-        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | should -be '4.1.2'
+        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | should -be '4.1.2.0'
     }
 
     It 'has az installed' {
