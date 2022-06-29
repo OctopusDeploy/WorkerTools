ad514ec3dfc4346637b5549489e4777dd695ba80
Yihao <yihao.wang@octopus.com>
2022-06-29 19:08:46 +1200

    Implement enthusiastic promotor

--- EnthusiasticPromotions/enthusiastic-promotor.tests.ps1
+++ EnthusiasticPromotions/enthusiastic-promotor.tests.ps1
@@ -1,125 +1,126 @@
-using module .\enthusiastic-promotor.psm1
-
 BeforeAll {
-    . .\enthusiastic-promotor.ps1
-    
-    $stagingEnvironment = "Environments-1"
-    $prodEnvironment = "Environments-2"
+    . $PSScriptRoot/enthusiastic-promotor.ps1
+
+    $stagingEnvironmentId = "Environments-1"
+    $prodEnvironmentId = "Environments-2"
+
+    function New-Release($releaseId, $projectId, $version, $created) { 
+        return New-Object PSObject -Property @{ 
+            ReleaseId = $releaseId
+            ProjectId = $projectId
+            Version = $version
+            Created = $created
+        } 
+    }
+
+    function New-Deployment($deploymentId, $releaseId, $environmentId) {
+        return New-Object PSObject -Property @{ 
+            DeploymentId = $deploymentId
+            ReleaseId = $releaseId
+            EnvironmentId = $environmentId 
+        } 
+    }
 }
 
 Describe "Select-PromotionCandidates" {
-    Context "with no parameters" {
-        It "returns nothing" {
-            # Act
-            $result = Select-PromotionCandidates ([Release[]] @()) ([Deployment[]] @()) $stagingEnvironment $prodEnvironment
+    It "ignores already promoted Releases" {
+        # Arrange
+        $releases = New-Release "Release-1" "Project-1" "0.0.1"
+        $deployments = @( 
+            New-Deployment "Deployment-1" "Release-1" $stagingEnvironmentId
+            New-Deployment "Deployment-2" "Release-1" $prodEnvironmentId
+        )
 
-            # Assert
-            $result.Count | Should -Be 0
-        }
+        # Act
+        $result = @(Select-PromotionCandidates $releases $deployments $stagingEnvironmentId $prodEnvironmentId)
+
+        # Assert
+        $result.Count | Should -Be 0
     }
 
-    Context "with a Release and no Deployments" {
-        It "returns nothing" {
-            # Arrange
-            $releases = [Release[]] @( [Release]::new("Release-1", "Project-1") )
-            $deployments = [Deployment[]] @()
-            
-            # Act
-            $result = Select-PromotionCandidates $releases $deployments $stagingEnvironment $prodEnvironment
+    It "includes unpromoted Releases" {
+        # Arrange
+        $releases = @( 
+            New-Release "Release-1" "Project-1" "0.0.1"
+            New-Release "Release-2" "Project-1" "0.0.2"
+        )
+        $deployments = @( 
+            New-Deployment "Deployment-1" "Release-1" $stagingEnvironmentId
+            New-Deployment "Deployment-2" "Release-1" $prodEnvironmentId
+            New-Deployment "Deployment-3" "Release-2" $stagingEnvironmentId
+        )
 
-            # Assert
-            $result.Count | Should -Be 0
-        }
+        # Act
+        $result = @(Select-PromotionCandidates $releases $deployments $stagingEnvironmentId $prodEnvironmentId)
+
+        # Assert
+        $result.Count | Should -Be 1
+        $result[0].ReleaseId | Should -Be "Release-2"
     }
 
-    Context "with no Releases and a Deployment" {
-        It "returns nothing" {
-            # Arrange
-            $releases = [Release[]] @()
-            $deployments = [Deployment[]] @( [Deployment]::new("Deployment-1", "Release-1", "Project-1") )
-            
-            # Act
-            $result = Select-PromotionCandidates $releases $deployments $stagingEnvironment $prodEnvironment
+    It "excludes superseded unpromoted Releases" {
+        # Arrange
+        $releases = @( 
+            New-Release "Release-1" "Project-1" "0.0.1" [DateTime]::Now.AddDays(-10)
+            New-Release "Release-2" "Project-1" "0.0.2" [DateTime]::Now.AddDays(-7)
+            New-Release "Release-3" "Project-1" "0.0.3" [DateTime]::Now.AddDays(-3)
+        )
+        $deployments = @( 
+            New-Deployment "Deployment-1" "Release-1" $stagingEnvironmentId
 
-            # Assert
-            $result.Count | Should -Be 0
-        }
-    }
-    Context "with valid Releases and Deployments" {
-        It "ignores already promoted Releases" {
-            # Arrange
-            $releases = [Release[]] @( [Release]::new("Release-1", "Project-1") )
-            $deployments = [Deployment[]] @( 
-                [Deployment]::new("Deployment-1", "Release-1", $stagingEnvironment)
-                [Deployment]::new("Deployment-2", "Release-1", $prodEnvironment)
-            )
+            New-Deployment "Deployment-2" "Release-2" $stagingEnvironmentId
+            New-Deployment "Deployment-3" "Release-2" $prodEnvironmentId
+            New-Deployment "Deployment-4" "Release-3" $stagingEnvironmentId
+        )
 
-            # Act
-            $result = Select-PromotionCandidates $releases $deployments $stagingEnvironment $prodEnvironment
+        # Act
+        $result = @(Select-PromotionCandidates $releases $deployments $stagingEnvironmentId $prodEnvironmentId)
 
-            # Assert
-            $result.Count | Should -Be 0
-        }
-
-        It "includes unpromoted Releases" {
-            # Arrange
-            $releases = [Release[]] @( 
-                [Release]::new("Release-1", "Project-1")
-                [Release]::new("Release-2", "Project-1")
-            )
-            $deployments = [Deployment[]] @( 
-                [Deployment]::new("Deployment-1", "Release-1", $stagingEnvironment)
-                [Deployment]::new("Deployment-2", "Release-1", $prodEnvironment)
-
-                [Deployment]::new("Deployment-3", "Release-2", $stagingEnvironment)
-            )
-
-            # Act
-            $result = Select-PromotionCandidates $releases $deployments $stagingEnvironment $prodEnvironment
-
-            # Assert
-            $result.Count | Should -Be 1
-            $result[0].ReleaseId | Should -Be "Release-2"
-        }
-
-        It "excludes superseded unpromoted Releases" {
-            # Arrange
-            $releases = [Release[]] @( 
-                [Release]::new("Release-1", "Project-1", [DateTime]::Now.AddDays(-10))
-                [Release]::new("Release-2", "Project-1", [DateTime]::Now.AddDays(-7))
-                [Release]::new("Release-3", "Project-1", [DateTime]::Now.AddDays(-3))
-            )
-            $deployments = [Deployment[]] @( 
-                [Deployment]::new("Deployment-1", "Release-1", $stagingEnvironment)
-
-                [Deployment]::new("Deployment-2", "Release-2", $stagingEnvironment)
-                [Deployment]::new("Deployment-3", "Release-2", $prodEnvironment)
-
-                [Deployment]::new("Deployment-4", "Release-3", $stagingEnvironment)
-            )
-
-            # Act
-            $result = Select-PromotionCandidates $releases $deployments $stagingEnvironment $prodEnvironment
-
-            # Assert
-            $result.Count | Should -Be 1
-            $result[0].ReleaseId | Should -Be "Release-3"
-        }
+        # Assert
+        $result.Count | Should -Be 1
+        $result[0].ReleaseId | Should -Be "Release-3"
     }
 }
 
-Describe "Select-ProductionDynamicWorkerRelease" {
-    Context "with no parameters" {
-        It "returns nothing" {
-            # Arrange
-            $releases = [Release[]] @()
-            $deployments = [Deployment[]] @()
+Describe "Select-CommonCachedVersions" {
+    It "selects the intersection of cached versions lists" {
+        # Arrange
+        $cachedVersionLists = @(
+            , @("0.0.1-ubuntu.18.04", "0.0.2-ubuntu.18.04")
+            , @("0.0.2-ubuntu.18.04", "0.0.3-ubuntu.18.04")
+            , @("0.0.2-ubuntu.18.04", "0.0.4-ubuntu.18.04")
+        )
 
-            # Act
-            $result = Select-ProductionDynamicWorkerRelease $releases $deployments
-            
-            # Assert
-            $result.Count | Should -Be 0
-        }
+        # Act
+        $result = @(Select-CommonCachedVersions $cachedVersionLists)
+
+        # Assert
+        $result.Count | Should -Be 1
+        $result[0] | Should -Be "0.0.2-ubuntu.18.04"
+    }
+}
+
+Describe "Select-CachedCandidates" {
+    It "selects candidates that are cached in production" {
+        # Arrange
+        $release2 = New-Release "Releases-2" "Projects-1" "0.0.2"
+        $release3 = New-Release "Releases-3" "Projects-1" "0.0.3"
+        $release4 = New-Release "Releases-4" "Projects-1" "0.0.4"
+
+        $promotionCandidates = @($release2, $release3, $release4)
+
+        $cachedWorkerToolsVersions = @(
+            "0.0.1-ubuntu.18.04"
+            "0.0.2-ubuntu.18.04"
+            "0.0.3-ubuntu.18.04"
+        )
+
+        # Act
+        $result = Select-CachedCandidates $promotionCandidates $cachedWorkerToolsVersions "ubuntu.18.04"
+
+        # Assert
+        $result.Count | Should -Be 2
+        $release2 | Should -BeIn $result
+        $release3 | Should -BeIn $result
     }
 }
