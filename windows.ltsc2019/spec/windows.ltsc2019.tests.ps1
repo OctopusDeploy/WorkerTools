740234d9d64bd3493b414c92958d98f3c1b859bd
veo.chen <veo.chen@octopus.com>
2023-04-17 14:47:34 +1200

    bad char

--- windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
+++ windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
@@ -61,7 +61,7 @@ Describe  'installed dependencies' {
     }
 
     It 'has kubelogin installed' {
-        kubelogin –version | Select-Object -First 1 | Should -match 'v0.0.25'
+        kubelogin --version | Should -match 'v0.0.25'
         $LASTEXITCODE | Should -be 0
     }
 
