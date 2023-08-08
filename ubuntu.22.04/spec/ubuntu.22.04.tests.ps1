667d0603611c7befae4eaa61085e9de10c3b5ee7
veo.chen <veo.chen@octopus.com>
2023-08-08 17:05:35 +1200

    fix test

--- ubuntu.22.04/spec/ubuntu.22.04.tests.ps1
+++ ubuntu.22.04/spec/ubuntu.22.04.tests.ps1
@@ -18,7 +18,7 @@ Describe  'installed dependencies' {
     }
 
     It 'has java installed' {
-        java --version | Should -beLike "*11.0.19*"
+        java --version | Should -beLike "*11.0.20*"
         $LASTEXITCODE | Should -be 0
     }
 
