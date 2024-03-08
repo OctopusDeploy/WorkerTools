b7b2799be0271471a7591cbe710906eef3d9e6d3
Justin Walsh <49404281+Justin-Walsh@users.noreply.github.com>
2024-03-08 14:49:42 -0500

    Update Gcloud test

--- ubuntu.22.04/spec/ubuntu.22.04.tests.ps1
+++ ubuntu.22.04/spec/ubuntu.22.04.tests.ps1
@@ -82,7 +82,7 @@ Describe  'installed dependencies' {
     }
     
     It 'has gke-gcloud-auth-plugin installed' {
-        gke-gcloud-auth-plugin --version | Select -First 1 | Should -be 'ubernetes v1.28.2-alpha+58ec6ae34b7dcd9699b37986ccb12b3bbac88f00'
+        gke-gcloud-auth-plugin --version | Select -First 1 | Should -be 'Kubernetes v1.28.2-alpha+58ec6ae34b7dcd9699b37986ccb12b3bbac88f00'
         $LASTEXITCODE | Should -be 0
     }
 
