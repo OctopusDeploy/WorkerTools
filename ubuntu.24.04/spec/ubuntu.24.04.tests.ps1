045b2e44c713621f338808d01acd83ccd64baa83
JT <justin.taylor@octopus.com>
2026-05-06 13:16:54 +1000

    Update installed tools for 24.04 (#117)

--- ubuntu.24.04/spec/ubuntu.24.04.tests.ps1
+++ ubuntu.24.04/spec/ubuntu.24.04.tests.ps1
@@ -5,12 +5,12 @@ Write-Host 'Running tests with Pester v'+$($pesterModules[0].Version)
 
 Describe  'installed dependencies' {
     It 'has Octopus.Client installed ' {
-        $expectedVersion = "14.3.1789"
+        $expectedVersion = "21.6.2652"
         [Reflection.AssemblyName]::GetAssemblyName("/Octopus.Client.dll").Version.ToString() | Should -match "$expectedVersion.0"
     }
 
     It 'has dotnet installed' {
-        dotnet --version | Should -match '8.0.\d+'
+        dotnet --version | Should -match '10.0.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
@@ -20,46 +20,46 @@ Describe  'installed dependencies' {
     }
 
     It 'has aws powershell module installed' {
-        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | should -be '4.1.734'
+        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | should -be '5.0.207'
     }
 
     It 'has az installed' {
         $output = (& az version) | convertfrom-json
-        $output.'azure-cli' | Should -be '2.67.0'
+        $output.'azure-cli' | Should -be '2.85.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has az powershell module installed' {
-        (Get-Module Az -ListAvailable).Version.ToString() | should -be '13.0.0'
+        (Get-Module Az -ListAvailable).Version.ToString() | should -be '15.5.0'
     }
 
     It 'has aws cli installed' {
-        aws --version 2>&1 | Should -match '2.24.10'
+        aws --version 2>&1 | Should -match '2.34.42'
     }
 
     It 'has node installed' {
-        node --version | Should -match '22.\d+.\d+'
+        node --version | Should -match '24.\d+.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubectl installed' {
-        kubectl version --client | Select-Object -First 1 | Should -match '1.32.\d+'
+        kubectl version --client | Select-Object -First 1 | Should -match '1.36.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubelogin installed' {
-        kubelogin --version | Select-Object -First  1 -Skip 1 | Should -match 'v0.1.6'
+        kubelogin --version | Select-Object -First  1 -Skip 1 | Should -match 'v0.2.17'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has helm installed' {
-        helm version | Should -match '3.16.4'
+        helm version | Should -match '3.20.2'
         $LASTEXITCODE | Should -be 0
     }
 
     # If the terraform version is not the latest, then `terraform version` returns multiple lines and a non-zero return code
     It 'has terraform installed' {
-        terraform version | Select-Object -First 1 | Should -match '1.10.4'
+        terraform version | Select-Object -First 1 | Should -match '1.15.1'
     }
 
     It 'has python3 installed' {
@@ -68,24 +68,24 @@ Describe  'installed dependencies' {
     }
 
     It 'has gcloud installed' {
-        gcloud --version | Select -First 1 | Should -be 'Google Cloud SDK 550.0.0'
+        gcloud --version | Select -First 1 | Should -be 'Google Cloud SDK 566.0.0'
         $LASTEXITCODE | Should -be 0
     }
     
     It 'has gke-gcloud-auth-plugin installed' {
         #We use belike here as the hash after the 'alpha+' changes and isn't that important
-        gke-gcloud-auth-plugin --version | Select -First 1 | Should -beLike 'Kubernetes v1.30.0*'
+        gke-gcloud-auth-plugin --version | Select -First 1 | Should -beLike 'Kubernetes v35.0.1*'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has octopus cli installed' {
-        octopus version | Should -match '2.20.1'
+        octopus version | Should -match '2.21.0'
         $LASTEXITCODE | Should -be 0
     }     
 
 
     It 'has eksctl installed' {
-        eksctl version | Should -match '0.220.0'
+        eksctl version | Should -match '0.226.0'
         $LASTEXITCODE | Should -be 0
     }
 
@@ -122,12 +122,12 @@ Describe  'installed dependencies' {
     It 'should have installed powershell core' {
         $output = & pwsh --version
         $LASTEXITCODE | Should -be 0
-        $output | Should -match '^PowerShell 7\.5\.4*'
+        $output | Should -match '^PowerShell 7\.6\.1*'
     }
 
     It 'should have installed argo cli' {
         $output = (& argocd version --client) -join "`n"
         $LASTEXITCODE | Should -be 0
-        $output | Should -Match '2.14.21'
+        $output | Should -Match '3.3.9'
     }
 }
