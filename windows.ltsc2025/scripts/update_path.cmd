386f61d76a0ef1c53c389d18d0d57912059485ae
Cal Luy <Cal.Luy@octopus.com>
2026-06-01 11:54:09 +1000

    Small fixes

--- windows.ltsc2025/scripts/update_path.cmd
+++ windows.ltsc2025/scripts/update_path.cmd
@@ -1,2 +1,2 @@
-setx /M path "%PATH%;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet;C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.20.3.2503\lib\net462\Octopus.Client.dll"
+setx /M path "%PATH%;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet;C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.21.11.2726\lib\net462\Octopus.Client.dll"
 
