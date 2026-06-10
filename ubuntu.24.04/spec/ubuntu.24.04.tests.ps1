fcb39b9b39fc9a4656904118f188a512c64f5076
JT <justin.taylor@octopus.com>
2026-06-10 16:59:58 +1000

    update python on Ubuntu to 3.14 (#130)
    * Bump python in Windows 2022
    
    * tighten Pester versions
    
    * Standardise on string matching for version tests where possible
    
    * Update test structure to better align with Windows containers
    
    * matching windows test script styling
    
    * simplify python to 3.14
    
    * remove distutils since it doesn't exist after 3.12

--- ubuntu.24.04/spec/ubuntu.24.04.tests.ps1
+++ ubuntu.24.04/spec/ubuntu.24.04.tests.ps1
@@ -6,123 +6,127 @@ Write-Host 'Running tests with Pester v'+$($pesterModules[0].Version)
 Describe  'installed dependencies' {
     It 'has Octopus.Client installed ' {
         $expectedVersion = "21.6.2652"
-        [Reflection.AssemblyName]::GetAssemblyName("/Octopus.Client.dll").Version.ToString() | Should -match "$expectedVersion.0"
+        [Reflection.AssemblyName]::GetAssemblyName("/Octopus.Client.dll").Version.ToString() | Should -Match "$expectedVersion.0"
     }
 
     It 'has dotnet installed' {
-        dotnet --version | Should -match '10.0.\d+'
+        [string](& dotnet --version) | Should -Match '10.0.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has java installed' {
-        java --version | Should -beLike "* 21.0.*"
+        $output = & java --version
+        [string]$output | Should -Match '21\.0\.'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has aws powershell module installed' {
-        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | should -be '5.0.207'
+        (Get-Module AWSPowerShell.NetCore -ListAvailable).Version.ToString() | Should -Be '5.0.207'
     }
 
     It 'has az installed' {
-        $output = (& az version) | convertfrom-json
-        $output.'azure-cli' | Should -be '2.85.0'
+        $output = (& az version) | ConvertFrom-Json
+        $output.'azure-cli' | Should -Be '2.85.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has az powershell module installed' {
-        (Get-Module Az -ListAvailable).Version.ToString() | should -be '15.5.0'
+        (Get-Module Az -ListAvailable).Version.ToString() | Should -Be '15.5.0'
     }
 
     It 'has aws cli installed' {
-        aws --version 2>&1 | Should -match '2.34.42'
+        $output = & aws --version 2>&1
+        [string]$output | Should -Match '2.34.42'
     }
 
     It 'has node installed' {
-        node --version | Should -match '24.\d+.\d+'
+        [string](& node --version) | Should -Match '24.\d+.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubectl installed' {
-        kubectl version --client | Select-Object -First 1 | Should -match '1.36.\d+'
+        $output = & kubectl version --client
+        [string]$output | Should -Match '1.36.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubelogin installed' {
-        kubelogin --version | Select-Object -First  1 -Skip 1 | Should -match 'v0.2.17'
+        $output = & kubelogin --version
+        [string]$output | Should -Match 'v0.2.17'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has helm installed' {
-        helm version | Should -match '3.20.2'
+        [string](& helm version) | Should -Match '3.20.2'
         $LASTEXITCODE | Should -be 0
     }
 
-    # If the terraform version is not the latest, then `terraform version` returns multiple lines and a non-zero return code
     It 'has terraform installed' {
-        terraform version | Select-Object -First 1 | Should -match '1.15.1'
+        $output = & terraform version
+        [string]$output | Should -Match '1.15.1'
     }
 
     It 'has python3 installed' {
-        python3 --version | Should -match '3.12.3'
+        [string](& python3 --version) | Should -Match '3.14.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has gcloud installed' {
-        gcloud --version | Select -First 1 | Should -be 'Google Cloud SDK 566.0.0'
+        $output = & gcloud --version
+        [string]$output | Should -Match '566.0.0'
         $LASTEXITCODE | Should -be 0
     }
-    
+
     It 'has gke-gcloud-auth-plugin installed' {
-        #We use belike here as the hash after the 'alpha+' changes and isn't that important
-        gke-gcloud-auth-plugin --version | Select -First 1 | Should -beLike 'Kubernetes v35.0.1*'
+        $output = & gke-gcloud-auth-plugin --version
+        [string]$output | Should -Match 'Kubernetes v'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has octopus cli installed' {
-        octopus version | Should -match '2.21.0'
+        [string](& octopus version) | Should -Match '2.21.0'
         $LASTEXITCODE | Should -be 0
-    }     
-
+    }
 
     It 'has eksctl installed' {
-        eksctl version | Should -match '0.226.0'
+        [string](& eksctl version) | Should -Match '0.226.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has ecs-cli installed' {
-        ecs-cli --version | Should -match '1.21.0'
+        [string](& ecs-cli --version) | Should -Match '1.21.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has mvn installed' {
-        mvn --version | out-null
+        & mvn --version | Out-Null
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has gradle installed' {
-        gradle --version | out-null
+        & gradle --version | Out-Null
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has aws-iam-authenticator installed' {
-        aws-iam-authenticator version | out-null
+        & aws-iam-authenticator version | Out-Null
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has istioctl installed' {
-        istioctl version --remote=false | out-null
+        & istioctl version --remote=false | Out-Null
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has linkerd installed' {
-        linkerd version --client | out-null
+        & linkerd version --client | Out-Null
         $LASTEXITCODE | Should -be 0
     }
 
     It 'should have installed powershell core' {
         $output = & pwsh --version
         $LASTEXITCODE | Should -be 0
-        $output | Should -match '^PowerShell 7\.6\.1*'
+        $output | Should -Match '^PowerShell 7\.6\.1*'
     }
 
     It 'should have installed argo cli' {
