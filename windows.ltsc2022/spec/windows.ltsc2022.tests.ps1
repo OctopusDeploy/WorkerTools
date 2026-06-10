73b22873513da5cca0ba2ae7dfe7fc32ee92138b
JT <justin.taylor@octopus.com>
2026-06-10 16:48:41 +1000

    fix: python + test fixes for windows (#129)
    * Bump python in Windows 2022
    
    * tighten Pester versions
    
    * Standardise on string matching for version tests where possible

--- windows.ltsc2022/spec/windows.ltsc2022.tests.ps1
+++ windows.ltsc2022/spec/windows.ltsc2022.tests.ps1
@@ -17,91 +17,97 @@ Describe  'installed dependencies' {
     }
 
     It 'has dotnet installed' {
-        dotnet --version | Should -Match '8.0.\d+'
+        [string](& dotnet --version) | Should -Match '8.0.\d+'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has java installed' {
-        java -version 2>&1 | Select-String -Pattern '25' | Should -BeLike "*25*"
+        $output = & java -version 2>&1
+        [string]$output | Should -Match '25'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has az installed' {
-      $output = (& az version) | convertfrom-json
-      $output.'azure-cli' | Should -Be '2.81.0'
-      $LASTEXITCODE | Should -be 0
+        $output = (& az version) | ConvertFrom-Json
+        $output.'azure-cli' | Should -Be '2.81.0'
+        $LASTEXITCODE | Should -be 0
     }
-    
+
     It 'has az powershell module installed' {
-        (Get-Module Az -ListAvailable).Version.ToString() | should -be '15.1.0'
+        (Get-Module Az -ListAvailable).Version.ToString() | Should -Be '15.1.0'
     }
 
     It 'has aws cli installed' {
-      aws --version 2>&1 | Should -Match '2.32.28'
+        $output = & aws --version 2>&1
+        [string]$output | Should -Match '2.32.28'
     }
 
     It 'has aws powershell installed' {
-      Import-Module AWSPowerShell.NetCore
-      Get-AWSPowerShellVersion | Should -Match '5.0.128'
+        Import-Module AWSPowerShell.NetCore
+        [string](& Get-AWSPowerShellVersion) | Should -Match '5.0.128'
     }
-    
-    # There is no version command for aws-iam-authenticator, so we just check for the installed version.
+
     It 'has aws-iam-authenticator installed' {
-        Test-Path 'C:\ProgramData\chocolatey\bin\aws-iam-authenticator.exe' | should -be $true
+        Test-Path 'C:\ProgramData\chocolatey\bin\aws-iam-authenticator.exe' | Should -Be $true
     }
 
     It 'has node installed' {
-        node --version | Should -Match '24.12.0'
+        [string](& node --version) | Should -Match '24.12.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubectl installed' {
-        kubectl version --client | Select-String -Pattern "1.35.0" | Should -BeLike "Client Version: v1.35.0"
+        $output = & kubectl version --client
+        [string]$output | Should -Match '1.35.0'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has kubelogin installed' {
-        kubelogin --version | Select-Object -First  1 -Skip 1 | Should -match 'v0.2.13'
+        $output = & kubelogin --version
+        [string]$output | Should -Match 'v0.2.13'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has helm installed' {
-        helm version | Should -Match '3.20.2'
+        [string](& helm version) | Should -Match '3.20.2'
         $LASTEXITCODE | Should -be 0
     }
 
-    # If the terraform version is not the latest, then `terraform version` returns multiple lines and a non-zero return code
     It 'has terraform installed' {
-        terraform version | Select-Object -First 1 | Should -Match '1.14.3'
+        $output = & terraform version
+        [string]$output | Should -Match '1.14.3'
     }
 
     It 'has python installed' {
-        python --version | Should -Match '3.14.2'
+        [string](& python --version) | Should -Match '3.14.5'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has gcloud installed' {
-        gcloud --version | Select-String -Pattern "550.0.0" | Should -BeLike "Google Cloud SDK 550.0.0"
+        $output = & gcloud --version
+        [string]$output | Should -Match '550.0.0'
         $LASTEXITCODE | Should -be 0
     }
-    
+
+    # Version follows gcloud SDK bundled plugin; pin loosely to avoid drift.
     It 'has gke-gcloud-auth-plugin installed' {
-        gke-gcloud-auth-plugin --version | Select -First 1 | Should -BeLike "Kubernetes v1.30.0*"
+        $output = & gke-gcloud-auth-plugin --version
+        [string]$output | Should -Match 'Kubernetes v'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has octopus cli installed' {
-        octopus version | Should -Match '2.20.1'
+        [string](& octopus version) | Should -Match '2.20.1'
         $LASTEXITCODE | Should -be 0
-    }    
+    }
 
     It 'has octo installed' {
-        octo --version | Should -Match '9.1.7'
+        [string](& octo --version) | Should -Match '9.1.7'
         $LASTEXITCODE | Should -be 0
     }
 
     It 'has eksctl installed' {
-        eksctl version | Should -Match '0.221.0'
+        [string](& eksctl version) | Should -Match '0.221.0'
         $LASTEXITCODE | Should -be 0
     }
 
@@ -120,7 +126,7 @@ Describe  'installed dependencies' {
     It 'should have installed git' {
         $output = & git --version
         $LASTEXITCODE | Should -be 0
-        $output | Should -Match '2.52.0'
+        [string]$output | Should -Match '2.52.0'
     }
 
     It 'should have installed argo cli' {
